4a9867e8307c4b7472bdcf617f089e57edefdea9
yak3d <yak@yak3d.com>
2025-05-07 20:02:50 -0400

    re-enable issue[id]

--- src/app/[locale]/issues/[id]/page.tsx
+++ src/app/[locale]/issues/[id]/page.tsx
@@ -66,46 +66,46 @@ export default async function IssueView(props: IIssueViewProps) {
 
     return (
         <div>
-            {/*<h1>Issue #{props.params.id}</h1>*/}
-            {/*<BackButton href='/issues' label="Back to Issue List" icon={mdiArrowLeft} />*/}
-            {/*<h2 className={`mt-4`}>{issue?.title}</h2>*/}
-            {/*<div className="grid grid-flow-row grid-cols-3 mb-2 gap-1 lg:gap-4 border-2 border-black py-2 px-8 bg-[#2f4dd445]">*/}
-            {/*    <div className='flex items-center'><StatusLabel label={issue?.state!}/></div>*/}
-            {/*    <div>*/}
-            {/*        <a href={issue?.NexusMods ? `https://nexusmods.com/users/${issue.NexusMods.memberId}` : `https://github.com/${issue?.author.login}`}>*/}
-            {/*            <div className='flex flex-row align-middle gap-2 content-center items-center text-center'>*/}
-            {/*                <div className='hidden lg:flex'>Reporter: </div>*/}
-            {/*                <UserAvatar nexusMods={issue?.NexusMods} githubUser={issue?.author} /> */}
-            {/*                <div className='hidden lg:flex'>{issue?.NexusMods?.name ?? issue?.author.login}</div>*/}
-            {/*            </div>*/}
-            {/*        </a>*/}
-            {/*    </div>*/}
-            {/*    <div className='flex items-center'><RelativeDate date={issue?.createdAt ?? 0} icon={mdiCreation} label={'Created'} /></div>*/}
-            {/*</div>*/}
-            {/*<div className='[&>h2]:mt-4 [&>h2]:mb-0.5 [&>h2]:font-orbitron border-2 border-black py-2 px-8'>*/}
-            {/*<IssueBody body={issue?.body !== "" ? issue?.body! : '*No issue description provided.*'} />*/}
-            {/*{ */}
-            {/*(issue?.labels.nodes ?? []).length */}
-            {/*? <div className='my-2 border-t-2 pt-2'>{(issue?.labels.nodes ?? []).map((l) => <IssueLabel label={l} key={l.id} />)}</div> */}
-            {/*: null*/}
-            {/*}*/}
-            {/*</div>*/}
-            {/*<div className="grid grid-flow-row grid-cols-3 mt-2 gap-4 border-2 border-black py-2 px-8 bg-[#2f4dd445] items-center">*/}
-            {/*    <div>*/}
-            {/*        <a href={issue?.url} target='_blank'>*/}
-            {/*            <Image */}
-            {/*                src={GitHub}*/}
-            {/*                width={18}*/}
-            {/*                height={18}*/}
-            {/*                alt='GitHub Logo'*/}
-            {/*                title='View on GitHub'*/}
-            {/*                className='inline mr-2'*/}
-            {/*            />*/}
-            {/*            GitHub ↗*/}
-            {/*        </a>*/}
-            {/*    </div>*/}
-            {/*    <div className='col-start-3'><RelativeDate date={issue?.updatedAt ?? 0} icon={mdiUpdate} label={'Updated'} /></div>*/}
-            {/*</div>*/}
+            <h1>Issue #{props.params.id}</h1>
+            <BackButton href='/issues' label="Back to Issue List" icon={mdiArrowLeft} />
+            <h2 className={`mt-4`}>{issue?.title}</h2>
+            <div className="grid grid-flow-row grid-cols-3 mb-2 gap-1 lg:gap-4 border-2 border-black py-2 px-8 bg-[#2f4dd445]">
+                <div className='flex items-center'><StatusLabel label={issue?.state!}/></div>
+                <div>
+                    <a href={issue?.NexusMods ? `https://nexusmods.com/users/${issue.NexusMods.memberId}` : `https://github.com/${issue?.author.login}`}>
+                        <div className='flex flex-row align-middle gap-2 content-center items-center text-center'>
+                            <div className='hidden lg:flex'>Reporter: </div>
+                            <UserAvatar nexusMods={issue?.NexusMods} githubUser={issue?.author} />
+                            <div className='hidden lg:flex'>{issue?.NexusMods?.name ?? issue?.author.login}</div>
+                        </div>
+                    </a>
+                </div>
+                <div className='flex items-center'><RelativeDate date={issue?.createdAt ?? 0} icon={mdiCreation} label={'Created'} /></div>
+            </div>
+            <div className='[&>h2]:mt-4 [&>h2]:mb-0.5 [&>h2]:font-orbitron border-2 border-black py-2 px-8'>
+            <IssueBody body={issue?.body !== "" ? issue?.body! : '*No issue description provided.*'} />
+            {
+            (issue?.labels.nodes ?? []).length
+            ? <div className='my-2 border-t-2 pt-2'>{(issue?.labels.nodes ?? []).map((l) => <IssueLabel label={l} key={l.id} />)}</div>
+            : null
+            }
+            </div>
+            <div className="grid grid-flow-row grid-cols-3 mt-2 gap-4 border-2 border-black py-2 px-8 bg-[#2f4dd445] items-center">
+                <div>
+                    <a href={issue?.url} target='_blank'>
+                        <Image
+                            src={GitHub}
+                            width={18}
+                            height={18}
+                            alt='GitHub Logo'
+                            title='View on GitHub'
+                            className='inline mr-2'
+                        />
+                        GitHub ↗
+                    </a>
+                </div>
+                <div className='col-start-3'><RelativeDate date={issue?.updatedAt ?? 0} icon={mdiUpdate} label={'Updated'} /></div>
+            </div>
             {/*<CommentContainer id={issue?.id!} number={issueId}  />*/}
         </div>
     )
